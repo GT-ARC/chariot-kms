58fd89f8ad2511701a08c96191391a4d3e5daeee
Brotsalat <tobimp@googlemail.com>
2020-05-19 15:54:21 +0200

    - fix bug in human controller, unique field was incorrect

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-sudo docker-compose -f docker-compose.yml  down
-sudo docker-compose -f docker-compose.yml  up -d --build --force-recreate kafka kafka2 kafka3 mongo flask
+sudo docker-compose -f docker-compose.yml down
+sudo docker-compose -f docker-compose.yml up -d --build --force-recreate kafka kafka2 kafka3 mongo flask
