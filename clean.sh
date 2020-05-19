b896fe2e8a57249131e27773792b54e0a63b5623
Brotsalat <tobimp@googlemail.com>
2020-05-19 14:46:25 +0200

    updated run configurations

--- clean.sh
+++ clean.sh
@@ -1,4 +1,5 @@
 sudo docker-compose -f docker-compose.global.yml -f docker-compose.local.yml down
 
 sudo rm -rf ./db/*
-sudo docker-compose -f docker-compose.global.yml -f docker-compose.local.yml up -d --build --force-recreate   kafka kafka2 kafka3 kafka_manager mongo proxy redis flask
+sudo docker-compose -f docker-compose.global.yml -f docker-compose.local.yml up -d --build --force-recreate   kafka kafka2 kafka3 mongo proxy flask
+
--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 sudo docker-compose -f docker-compose.global.yml -f docker-compose.local.yml down
-sudo docker-compose -f docker-compose.global.yml -f docker-compose.local.yml up -d --build --force-recreate   kafka kafka2 kafka3 kafka_manager mongo proxy redis flask
+sudo docker-compose -f docker-compose.global.yml -f docker-compose.local.yml up -d --build --force-recreate   kafka kafka2 kafka3 mongo proxy flask
